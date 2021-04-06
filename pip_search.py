5f625b607d77e02c2109986e2f21a05c680d410b
damirk <damir.kucic@better.care>
2021-04-06 11:03:20 +0200

    Working

--- pip_search.py
+++ pip_search.py
@@ -2,39 +2,63 @@ import requests
 import re
 import json
 import sys
-import argparse
 
 
-def get_packagenames(package_name, substring = True):
+
+def get_packagenames(package_name, substring_match = True):
     """
     Search for package with full or partial package name
     """
     url = 'https://pypi.org/simple/'
     matches = []
+
     response = requests.get(url, stream=True)
     for line in response.iter_lines():
         p_name = (str(line, 'UTF-8'))
-        if substring == True:
+        
+        if substring_match == True:
             if p_name.find(package_name) > 0:
-                p_name_arr = p_name.split('>')
-                matches.append(p_name_arr[1][:-5])
-        else:
-            p_match = re.search(r"\b"+package_name+r"\b", p_name)
-            if p_match is not None:
-                p_name_arr = p_name.split('>')
-                matches.append(p_name_arr[1][:-5])
-                
-    return matches
+                p_name_arr = p_name.split(r'/">')
+                matches.append(p_name_arr[1][:-4])
 
-def package_description(package_name):
+        
+        if substring_match == False:
+            p_match = re.findall('\\b' + package_name + '\\b', p_name)
+            if len(p_match) > 0:
+                p_name_arr = p_name.split(r'/">')
+                matches.append(p_name_arr[1][:-4])                
+                
+    for match in matches:
+       print(match)
+
+
+def get_package_description(package_name):
     url = 'https://pypi.org/pypi/{}/json'.format(package_name) 
     response = requests.get(url)
     package_dict = json.loads(response.text)
     
-    return(package_dict['info']['description'])
+    print(package_dict['info']['description'])
     
 
 if __name__ == '__main__':
+
+    help_message = 'Please enter -p (-e) package_name for package list or -d for package_description.' 
+    if sys.argv[1] == '-p' and len(sys.argv) == 3:
+       get_packagenames(sys.argv[2])
+    
+    elif sys.argv[1] == '-p' and sys.argv[2] == '-e' and len(sys.argv) == 4: 
+       get_packagenames(sys.argv[3], substring_match=False)
+
+    elif sys.argv[1] == '-d' and len(sys.argv) == 3:
+        get_package_description(sys.argv[2])
+
+    else:
+        print(len(sys.argv))
+        print(sys.argv)
+    
+    
+    
+    
 
 
 
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+
+if sys.argv[1] == 'first':
+    print(len(sys.argv))
