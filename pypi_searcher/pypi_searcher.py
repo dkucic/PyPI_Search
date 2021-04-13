35fee5c1ddb7fd6723b9682bdd3ddd9044bb8200
damirk <damir.kucic@better.care>
2021-04-14 00:31:31 +0200

    Did some refactoring with decorators.

--- pypi_searcher/pypi_searcher.py
+++ pypi_searcher/pypi_searcher.py
@@ -5,16 +5,33 @@ import re
 import json
 import sys
 
+# decorator for modifying function behaviors based on weather this file is run
+# directly as script (results are printed) or as module (results are returned).
+def print_or_return(func):  
+    def wrapper(package_name):
+        values = func(package_name)
+        if __name__ == '__main__':
+            if isinstance(values, list):
+                for value in values:
+                    print(value)
+            else:
+                print(values)
+        else:
+            return values
+    return wrapper
 
+
+@print_or_return
 def get_package_monthy_downloads(package_name):
     url = 'https://pypistats.org/api/packages/{}/recent'.format(package_name)
     response = requests.get(url)
     package_dict = response.json()
-    
-    print (package_dict['data']['last_month'])
+
+    return package_dict['data']['last_month']
 
 
-def get_packagenames(package_name, substring_match = True):
+@print_or_return
+def get_packagenames(package_name):
     """
     Search for package with full or partial package name
     """
@@ -24,65 +41,51 @@ def get_packagenames(package_name, substring_match = True):
     response = requests.get(url, stream=True)
     for line in response.iter_lines():
         p_name = (str(line, 'UTF-8'))
-        
-        if substring_match == True:
-            if p_name.find(package_name) > 0:
-                p_name_arr = p_name.split(r'/">')
-                matches.append(p_name_arr[1][:-4])
+    
+        if p_name.find(package_name) > 0:
+            p_name_arr = p_name.split(r'/">')
+            matches.append(p_name_arr[1][:-4])
 
-        
-        if substring_match == False:
-            p_match = re.findall('\\b' + package_name + '\\b', p_name)
-            if len(p_match) > 0:
-                p_name_arr = p_name.split(r'/">')
-                matches.append(p_name_arr[1][:-4])                
-                
-    for match in matches:
-       print(match)
+    return matches
 
 
+@print_or_return
 def get_package_description(package_name):
-    url = 'https://pypi.org/pypi/{}/json'.format(package_name) 
+    url = 'https://pypi.org/pypi/{}/json'.format(package_name)
     response = requests.get(url)
     package_dict = response.json()
-    print(package_dict['info']['description'])
+
+    return package_dict['info']['description']
 
 
-def pypi_search():
-    help_message = ('Please enter ./pip_search -p (-e) package_name for package list, -d for package_description'
+def pypi_search(*argv):
+    help_message = ('Please enter ./pip_search -p package_name for package list, -d for package_description'
                     'or -n for number of monthly package downloads')
-    try:
-        
-        if sys.argv[1] == '-p' and len(sys.argv) == 3:
-           get_packagenames(sys.argv[2])
-        
-        elif sys.argv[1] == '-p' and sys.argv[2] == '-e' and len(sys.argv) == 4: 
-           get_packagenames(sys.argv[3], substring_match=False)
-    
-        elif sys.argv[1] == '-d' and len(sys.argv) == 3:
-            get_package_description(sys.argv[2])
 
-        elif sys.argv[1] == '-n' and len(sys.argv) == 3:
-            get_package_monthy_downloads(sys.argv[2])
-        
+    if __name__ == '__main__':
+        argv = sys.argv
+
+    try:
+
+        if argv[1] == '-p' and len(argv) == 3:
+           get_packagenames(argv[2])
+
+        elif argv[1] == '-p' and len(argv) == 3:
+           get_packagenames(argv[3])
+
+        elif argv[1] == '-d' and len(argv) == 3:
+            get_package_description(argv[2])
+
+        elif argv[1] == '-n' and len(argv) == 3:
+            get_package_monthy_downloads(argv[2])
+
         else:
             print(help_message)
 
     except:
         print(help_message)
-        
-    
-        
+
+
+
 if __name__ == '__main__':
     pypi_search()
-
-
-
-
-
-    
-    
-    
-   
-
-   
